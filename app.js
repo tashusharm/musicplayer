ae4318f3193a5828dcbc859f8d8b2cb4f775e88a
tashusharm <tashu6382@gmail.com>
2024-05-31 20:18:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'cover 5.png'
+    },
+]
